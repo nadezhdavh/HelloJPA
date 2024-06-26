f3ad89832d3cf7c90933ea68d515b4a7503c9ea6
Nadezhda <nadezhdavhristeva@gmail.com>
2024-06-26 22:31:04 +0300

    Initial commit

---
+++ src/main/java/com/academy/demo/HelloSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.academy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/academy/demo/persistance/entity/Transfer.java
@@ -0,0 +1,29 @@
+package com.academy.demo.persistance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+    private LocalDateTime transferDate;
+
+    @ManyToOne
+    @JoinColumn(name = "source_account_id")
+    private Account sourceAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_account_id")
+    private Account destinationAccount;
+}
