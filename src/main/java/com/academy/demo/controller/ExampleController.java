b27e270f9ad26a895e88de03824323d02526c571
Nadezhda <nadezhdavhristeva@gmail.com>
2024-07-10 00:32:37 +0300

    principle ...

--- src/main/java/com/academy/demo/controller/ExampleController.java
+++ src/main/java/com/academy/demo/controller/ExampleController.java
@@ -4,18 +4,20 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class ExampleController {
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String admin() {
-        return "Hello Admin";
+    public String admin(Principal principal) {
+        return String.format("Hello %s!", principal.getName());
     }
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public String user() {
-        return "Hello User";
+    public String user(Principal principal) {
+        return String.format("Hello %s!", principal.getName());
     }
 }
